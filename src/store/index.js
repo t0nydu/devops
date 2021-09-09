f79d5e15041cd970df8d7446d6d3d9ef49b4e029
t0nydu <tony@dulunix.com>
2021-09-09 22:18:41 +0800

    test

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    smg: 'hello',
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  mutations: {},
+  actions: {},
+  modules: {},
 })
